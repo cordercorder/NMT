fa6224c343034e9e6de28e2c85255a44eb60af91
cordercorder <2205722269@qq.com>
2020-06-23 23:17:33 +0800

    delete __pychae__ files, modify utils

--- utils/Vocab.py
+++ utils/Vocab.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from .process import normalizeString
+import pickle
 
 
 class Vocab:
@@ -19,12 +20,45 @@ class Vocab:
         self.end_token = end_token
         self.threshold = threshold
 
-        self.token2index = {}
-        self.index2token = {}
-        self.token_count = defaultdict(lambda: int())
+        self.__token2index = {}
+        self.__index2token = {}
+        self.__token_count = defaultdict(lambda: int())
+
+        self.unk_token = None
+        self.unk_token_index = None
 
         self.addtoken(self.start_token)
         self.addtoken(self.end_token)
+
+    def get_index(self, token):
+        """
+        A memory optimization method. If self.__token2index[token] is directly called, an item
+        will insert to token2index, but self.__token2index.get(token) will not
+        :param token: token to be encoded. type: str
+        :return: unique id of the token. type: int
+        """
+        return self.__token2index.get(token, self.token_not_found())
+
+    def get_token(self, index):
+        """
+        A memory optimization method. If self.__index2token[token] is directly called, an item
+        will insert to index2token, but self.__index2token.get(token) will not
+        :param index: unique id of the token. type: int
+        :return: the token which id is equal to index. type: str
+        """
+        return self.__index2token.get(index, self.index_not_found())
+
+    def token_not_found(self):
+        if self.unk_token_index is None:
+            raise Exception("Token not found")
+        else:
+            return self.unk_token_index
+
+    def index_not_found(self):
+        if self.unk_token is None:
+            raise Exception("Index not found")
+        else:
+            return self.unk_token
 
     def addtoken(self, token):
 
@@ -33,13 +67,13 @@ class Vocab:
         :return: None
         """
 
-        self.token_count[token] = self.token_count[token] + 1
+        self.__token_count[token] = self.__token_count[token] + 1
 
-        if token not in self.token2index:
+        if token not in self.__token2index:
 
-            num = len(self.token2index)
-            self.token2index[token] = num
-            self.index2token[num] = token
+            num = len(self.__token2index)
+            self.__token2index[token] = num
+            self.__index2token[num] = token
 
     def add_sentence(self, sentence):
 
@@ -64,22 +98,98 @@ class Vocab:
             for line in data:
                 self.add_sentence(line)
 
+    def add_unk(self, unk="UNK"):
+
+        """
+        Add unknown words
+        :return: None
+        """
+
+        self.unk_token = unk
+
+        self.__token_count = defaultdict(lambda: int(), {
+                                            k: value for k, value in self.__token_count.items()
+                                            if value >= self.threshold
+                                       })
+
+        self.__token_count[self.unk_token] = 1
+
+        self.__token2index = {}
+        self.__index2token = {}
+
+        for tok in self.__token_count:
+
+            if tok not in self.__token2index:
+                num = len(self.__token2index)
+                self.__token2index[tok] = num
+                self.__index2token[num] = tok
+
+        self.unk_token_index = self.__token2index[self.unk_token]
+
+    def save(self, save_path):
+        """
+        save the entity to disk
+        :param save_path: path to save the entity. type: str
+        :return: None
+        """
+        entity = {
+            "language_name": self.language_name,
+            "start_token": self.start_token,
+            "end_token": self.end_token,
+            "threshold": self.threshold,
+            "__token2index": self.__token2index,
+            "__index2token": self.__index2token,
+            "__token_count": dict(self.__token_count),
+            "unk_token": self.unk_token,
+            "unk_token_index": self.unk_token_index
+        }
+        with open(save_path, "wb") as f:
+            pickle.dump(entity, f)
+
+    @classmethod
+    def load(cls, load_path):
+        """
+        load the entity from disk
+        :param load_path: path to load the entity. type: str
+        :return: the load entity. type: object
+        """
+        with open(load_path, "rb") as f:
+            entity = pickle.load(f)
+            language_name = entity["language_name"]
+            start_token = entity["start_token"]
+            end_token = entity["end_token"]
+            threshold = entity["threshold"]
+            __token2index = entity["__token2index"]
+            __index2token = entity["__index2token"]
+            __token_count = defaultdict(lambda: int(), entity["__token_count"])
+            unk_token = entity["unk_token"]
+            unk_token_index = entity["unk_token_index"]
+
+            v = cls(language_name, start_token, end_token, threshold)
+
+            v.__token_count = __token_count
+            v.__index2token = __index2token
+            v.__token2index = __token2index
+            v.unk_token = unk_token
+            v.unk_token_index = unk_token_index
+            return v
+
     def __len__(self):
 
         """
         :return: the number of different tokens
         """
 
-        return len(self.token_count)
+        return len(self.__token_count)
 
     def __repr__(self):
 
         """
-        :return: randomly return 10 tokens if the number of tokens in self.token_count if larger than 10,
+        :return: randomly return 10 tokens if the number of tokens in self.__token_count if larger than 10,
                  else return all tokens
         """
 
-        tmp_key = list(self.token_count.keys())
+        tmp_key = list(self.__token_count.keys())
         if len(tmp_key) > 10:
             s = ", ".join(tmp_key[:10]) + "......"
         else:
@@ -90,20 +200,63 @@ class Vocab:
 
 if __name__ == "__main__":
 
+    def test(v):
+        print("------")
+        print(v.language_name)
+        print("------\n")
+
+        print("------")
+        print(v._Vocab__index2token)
+        print("------\n")
+
+        print("------")
+        print(v._Vocab__token2index)
+        print("------\n")
+
+        print("------")
+        print(v._Vocab__token_count)
+        print("------\n")
+
+        print("------")
+        print(v.start_token)
+        print("------\n")
+
+        print("------")
+        print(v.end_token)
+        print("------\n")
+
+        print("------")
+        print(v.threshold)
+        print("------\n")
+
+        print("------")
+        print(v.unk_token)
+        print("------\n")
+
+        print("------")
+        print(v.unk_token_index)
+        print("------\n")
+
+
     v = Vocab("en", "<s>", "<e>")
 
     v.addtoken("test1")
     v.addtoken("test2")
 
-    v.add_corpus("D:/jinrenren/NLP_study/codes/NMT\data/test_input.en")
+    v.add_corpus("D:/jinrenren/NLP_study/codes/NMT/data/test_input.en")
 
-    for tok in v.token2index:
-        print(tok)
-        print(v.token2index[tok])
+    # print(v.get_index("test"))
 
-    for index in v.index2token:
-        print(index)
-        print(v.index2token[index])
+    test(v)
 
-    print(v.index2token)
-    print(v.token2index)
+    v.add_unk()
+
+    print(v.get_index("test"))
+
+    v.save("D:/jinrenren/NLP_study/codes/NMT/data/vocab.en")
+
+    test(v)
+
+    v2 = Vocab.load("D:/jinrenren/NLP_study/codes/NMT/data/vocab.en")
+
+    test(v2)
