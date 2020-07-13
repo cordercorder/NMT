fix bugs that computation graph is destroyed before gradient calculation

models/S2S_attention.py
@@ -303,7 +303,7 @@ class S2S(nn.Module):
 
         return decoder_output
 
-    def train_batch(self, input_batch, target_batch, padding_value, criterion, use_teacher_forcing):
+    def train_batch(self, input_batch, target_batch, padding_value, criterion, optimizer, use_teacher_forcing):
 
         output = self(input_batch, target_batch, use_teacher_forcing)
 
@@ -323,4 +323,9 @@ class S2S(nn.Module):
 
             batch_loss[k-1] = loss.sum()
 
-        return batch_loss.sum()
+        optimizer.zero_grad()
+        batch_loss = batch_loss.sum()
+        batch_loss.backward()
+        optimizer.step()
+
+        return batch_loss.item()

models/S2S_basic.py
@@ -222,11 +222,11 @@ class S2S(nn.Module):
 
         return decoder_output
 
-    def train_batch(self, input_batch, target_batch, padding_value, criterion, use_teacher_forcing):
+    def train_batch(self, input_batch, target_batch, padding_value, criterion, optimizer, use_teacher_forcing):
 
         output = self(input_batch, target_batch, use_teacher_forcing)
 
-        batch_loss = torch.zeros(target_batch.size(0) - 1 , 1, device=input_batch.device)
+        batch_loss = torch.zeros(target_batch.size(0) - 1, 1, device=input_batch.device)
 
         for k in range(1, target_batch.size(0)):
             # tmp_output_batch: (batch_size, vocab_size)
@@ -242,4 +242,9 @@ class S2S(nn.Module):
 
             batch_loss[k-1] = loss.sum()
 
-        return batch_loss.sum()
+        optimizer.zero_grad()
+        batch_loss = batch_loss.sum()
+        batch_loss.backward()
+        optimizer.step()
+
+        return batch_loss.item()

trainer/multi_gpu_train.py
@@ -154,16 +154,11 @@ for i in range(args.start_epoch, args.end_epoch):
 
         try:
             batch_loss = s2s.module.train_batch(input_batch, target_batch, padding_value, criterion,
-                                                use_teacher_forcing_list[j])
+                                                optimizer, use_teacher_forcing_list[j])
         except RuntimeError:
             print(input_batch.size(), target_batch.size())
 
-
-        optimizer.zero_grad()
-        batch_loss.backward()
-        optimizer.step()
-
-        epoch_loss += batch_loss.item()
+        epoch_loss += batch_loss
         steps += 1
 
         batch_word_count = torch.ne(target_batch, padding_value).sum().item()
@@ -174,9 +169,8 @@ for i in range(args.start_epoch, args.end_epoch):
             if local_rank == 0:
                 torch.save(save_model(s2s, optimizer, args),
                            args.checkpoint + "_" + str(i) + "_" + str(steps))
-            batch_loss_value = batch_loss.item()
-            ppl = math.exp(batch_loss_value / batch_word_count)
-            print("Batch loss: {}, batch perplexity: {}, local rank: {}".format(batch_loss_value, ppl, local_rank))
+            ppl = math.exp(batch_loss / batch_word_count)
+            print("Batch loss: {}, batch perplexity: {}, local rank: {}".format(batch_loss, ppl, local_rank))
 
 
     epoch_loss /= word_count
@@ -186,4 +180,4 @@ for i in range(args.start_epoch, args.end_epoch):
     print("Epoch: {}, time: {} seconds, loss: {}, local rank: {}".format(i, time.time() - start_time, epoch_loss,
                                                                          local_rank))
 
-torch.save(save_model(s2s, args.attention_size, optimizer, args), args.checkpoint + "_rank{}".format(local_rank))
+torch.save(save_model(s2s, optimizer, args), args.checkpoint + "_rank{}".format(local_rank))

trainer/train.py
@@ -42,7 +42,10 @@ parser.add_argument("--threshold", default=0, type=int)
 parser.add_argument("--save_model_steps", default=0.3, type=float)
 parser.add_argument("--teacher_forcing_ratio", default=0.5, type=float)
 parser.add_argument("--mask_token", default="<mask>")
-parser.add_argument("--rebuild_vocab", default=True, type=bool)
+
+parser.add_argument("--rebuild_vocab", action="store_true", default=False)
+parser.add_argument("--normalize", action="store_true", default=False)
+parser.add_argument("--sort_sentence_by_length", action="store_true", default=False)
 
 args, unknown = parser.parse_known_args()
 
@@ -50,17 +53,20 @@ device = torch.device(args.device)
 
 src_data, src_vocab = load_corpus_data(args.src_path, args.src_language, args.start_token, args.end_token,
                                        args.mask_token, args.src_vocab_path, args.rebuild_vocab, args.unk,
-                                       args.threshold)
+                                       args.threshold, args.normalize)
 
 tgt_data, tgt_vocab = load_corpus_data(args.tgt_path, args.tgt_language, args.start_token, args.end_token,
                                        args.mask_token, args.tgt_vocab_path, args.rebuild_vocab, args.unk,
-                                       args.threshold)
+                                       args.threshold, args.normalize)
 
 print("Source language vocab size: {}".format(len(src_vocab)))
 print("Target language vocab size: {}".format(len(tgt_vocab)))
 
 
 assert len(src_data) == len(tgt_data)
+
+if args.sort_sentence_by_length:
+    src_data, tgt_data = sort_src_sentence_by_length(list(zip(src_data, tgt_data)))
 
 src_data, tgt_data = sort_src_sentence_by_length(list(zip(src_data, tgt_data)))
 
@@ -99,6 +105,8 @@ else:
 
     optimizer = torch.optim.Adam(s2s.parameters(), args.learning_rate)
 
+s2s.train()
+
 criterion = nn.CrossEntropyLoss(reduction="none")
 
 padding_value = src_vocab.get_index(args.mask_token)
@@ -127,13 +135,10 @@ for i in range(args.start_epoch, args.end_epoch):
     for j, (input_batch, target_batch) in enumerate(train_loader):
 
         batch_loss = s2s.train_batch(input_batch, target_batch, padding_value, criterion,
-                                     use_teacher_forcing_list[j])
+                                     optimizer, use_teacher_forcing_list[j])
 
-        epoch_loss += batch_loss.item()
+        epoch_loss += batch_loss
 
-        optimizer.zero_grad()
-        batch_loss.backward()
-        optimizer.step()
         steps += 1
 
         batch_word_count = target_batch.size(0) * target_batch.size(1)
@@ -143,9 +148,8 @@ for i in range(args.start_epoch, args.end_epoch):
         if steps % save_model_steps == 0:
             
             torch.save(save_model(s2s, optimizer, args), args.checkpoint + "_" + str(i) + "_" + str(steps))
-            batch_loss_value = batch_loss.item()
-            ppl = math.exp(batch_loss_value / batch_word_count)
-            print("Batch loss: {}, batch perplexity: {}".format(batch_loss_value, ppl))
+            ppl = math.exp(batch_loss / batch_word_count)
+            print("Batch loss: {}, batch perplexity: {}".format(batch_loss, ppl))
 
 
     epoch_loss /= word_count